1ccef5077ce448c17ee4bd957ff4a02277271fa0
kattak <katrina.devaney@gmail.com>
2017-01-31 19:05:49 -0800

    Try JQuery laying out of emojidivs

--- script.js
+++ script.js
@@ -1,6 +1,32 @@
 // console.log($('.emojibox'));
 $(document).ready(function(){
 
+//attempt to layout emoji divs
+
+var emojis = "abcd".split("");
+
+//Create a div for each emoji
+emojis.forEach(function(elem,i){
+  var puppy = "🐶";
+//  var emojiary = "🐶😀🍉🐓🏝️".split("");
+//  emojiary does not work
+
+	 //add div to emojibox container div
+    var tmpdiv = document.createElement("div");
+
+    //append emoji div
+    var embox =  document.getElementById("emojibox")
+    embox.appendChild(tmpdiv);
+
+    //set div's class and id
+    tmpdiv.className = "emoji unselected";
+    tmpdiv.id = "emoji" + i;
+
+    //try adding text in div
+    tmpdiv.innerHTML = puppy;
+})
+
+
   $(".unselected").click(function(){
     console.log("clicked!");
     $(this).toggleClass('selected unselected');
